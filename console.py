95e50783ae850c44cfc80626ff5f632e3850bf02
Lilwm <lmiiri95@gmail.com>
2022-11-14 07:44:14 +0300

    fix indentation issues

--- console.py
+++ console.py
@@ -116,11 +116,12 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, line):
-         """Creates a new instance of BaseModel, saves it
-         Exceptions:
-            SyntaxError: when there is no args given
-            NameError: when there is no object taht has the name
-         """
+        """
+        Creates a new instance of BaseModel, saves it
+        Exceptions: 
+        SyntaxError: when there is no args given
+        NameError: when there is no object taht has the name
+        """
         try:
             if not line:
                 raise SyntaxError()
